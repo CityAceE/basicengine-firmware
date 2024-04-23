abf741c2557491fd44527d6ffb5a169203301110
Ulrich Hecht <uli@basicengine.org>
2024-04-23 17:08:58 +0200

    compat: add getline() for Win32

--- ttbasic/compat.c
+++ ttbasic/compat.c
@@ -37,6 +37,56 @@ int setenv(const char *name, const char *value, int overwrite)
     return 0;
 }
 
+typedef intptr_t ssize_t;
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
+    int c;
+
+    if (lineptr == NULL || stream == NULL || n == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    c = getc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+
+    if (*lineptr == NULL) {
+        *lineptr = malloc(128);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+        *n = 128;
+    }
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
+                return -1;
+            }
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        ((unsigned char *)(*lineptr))[pos ++] = c;
+        if (c == '\n') {
+            break;
+        }
+        c = getc(stream);
+    }
+
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+
 /* Hash character pairs so a small shift table can be used.  All bits of
    p[0] are included, but not all bits from p[-1].  So if two equal hashes
    match on p[-1], p[0] matches too.  Hash collisions are harmless and result
--- ttbasic/compat.h
+++ ttbasic/compat.h
@@ -8,6 +8,7 @@ void *memrchr(const void *s, int c_in, size_t n);
 #ifdef _WIN32
 void *memmem(const void *haystack, size_t hs_len, const void *needle, size_t ne_len);
 int setenv(const char *name, const char *value, int overwrite);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 #endif
 
 #ifdef __cplusplus
